0fcb23ab23bd655d7b6ec18fd2c8accbd4b87ded
karlonautti <karri.salonen@myy.haaga-helia.fi>
2026-09-05 20:52:37 +0300

    Modified pom.xml, Book.java, and added BookRepository.java

--- src/main/java/backend/bookstore/domain/Book.java
+++ src/main/java/backend/bookstore/domain/Book.java
@@ -1,6 +1,16 @@
 package backend.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     private String title;
     private String author;
